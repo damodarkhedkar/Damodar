b4687d401e37b1cdc1e3e7fcd53fff65c697aed2
damodarkhedkar <36823228+damodarkhedkar@users.noreply.github.com>
2020-02-08 16:49:49 +0530

    This is an E-Ticketing Application

---
+++ E-Ticketing System/E-Ticketing System/validation.js
@@ -0,0 +1,11 @@
+﻿function pass_val(pass)
+{
+    var regx = "(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}";
+    var data = document.getElementById(pass);
+    if (regx.test(data.value)) {
+        window.("login success");
+        return true;
+    } 
+    else
+          return false;
+}
