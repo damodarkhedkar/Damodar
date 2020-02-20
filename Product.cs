6cf39a062736028db1b84613b35cfa274e653966
damodarkhedkar <36823228+damodarkhedkar@users.noreply.github.com>
2020-02-20 16:27:13 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product
+{
+    class Product
+    {
+        int id;
+        string name;
+        double price;
+        Categary categary;
+
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public double Price { set; get; }
+        public Categary Cate { get; set; }
+
+        public void ShowProduct()
+        {
+            Console.WriteLine("Product Name: {0} Id: {1} Price: {2} Catagary Type: {3} ID: {4}",Name,Id,Price,Cate.Desc,Cate.Id);
+        }
+
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Categary c = new Categary { Desc="Hr",Id=12};
+            Product p = new Product {Id=1234,Name="SMX",Price=1000.25,Cate=c};
+            p.ShowProduct();
+            Console.Read();
+        }
+    }
+}
